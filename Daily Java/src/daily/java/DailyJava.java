81f7758be4e7c4f03630e4d0b0af4b4212bf1fd7
TomBu11 <73280868+TomBu11@users.noreply.github.com>
2020-10-26 16:12:15 +0000

    Daily Java

--- Daily Java/src/daily/java/DailyJava.java
+++ Daily Java/src/daily/java/DailyJava.java
@@ -7,13 +7,19 @@ public class DailyJava {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        double ndd;
         
-        System.out.println("Enter your weight (in kg)");
-        double weight = scan.nextInt();
+        System.out.println("Enter a number");
+        int n = scan.nextInt();
         
-        System.out.println("Enter your height (in metres)");
-        double height = scan.nextInt();
-        
-        System.out.println("Your BMI is " + (weight/(height* height)));
+        if (n % 3 == 0 && n % 5 == 0) {
+            System.out.println("Fizz Buzz");
+        } else if (n % 3 == 0) {
+            System.out.println("Fizz");
+        } else if (n % 5 == 0) {
+            System.out.println("Buzz");
+        } else {
+            System.out.println(n);
+        }
     }
 }
